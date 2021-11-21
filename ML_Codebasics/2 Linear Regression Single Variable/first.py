07ff6c819c69c9dbf4961703d284b44fbc6e2bb4
Md Imam Ahasan <emamahasane@gmail.com>
2021-11-22 02:05:20 +0600

    Add files via upload

---
+++ ML_Codebasics/2 Linear Regression Single Variable/first.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from sklearn import linear_model
+
+df = pd.read_csv("/Users/macbookair/Python/ML_youtube/homeprices.csv")
+
+print(df)
+
+# % matplotlib inline
+# m = plt.scatter(df.area, df.price)
+# print(m)
